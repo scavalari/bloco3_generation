69ec330b39b83a1f3ca7ae6252ab5d0523831947
Stephanie <ferreirasc09@gmail.com>
2022-02-14 22:07:04 -0300

    Blog-Pessoal REACT iniciando o projeto

---
+++ REACT/blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+        </>
+    );
+}
+
+export default Home;
